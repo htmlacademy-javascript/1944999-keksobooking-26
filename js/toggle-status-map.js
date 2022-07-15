c66fde6ca80169e10878fa7bf6409e4ea3385fbf
Antipov <vlad@antipovi.ru>
2022-07-15 10:28:04 +0300

    Включение и отключение элементов на странице

--- js/toggle-status-map.js
+++ js/toggle-status-map.js
@@ -1,23 +1,23 @@
-// const disablingMap = function () {
-//   const adForm = document.querySelector('.ad-form');
-//   const collectionFieldsetNotice = adForm.querySelectorAll('fieldset');
-//   const formMap = document.querySelector('.map__filters');
-//   const collectionOptionsMap = formMap.querySelectorAll('select');
+const disablingMap = function () {
+  const adForm = document.querySelector('.ad-form');
+  const collectionFieldsetNotice = adForm.querySelectorAll('fieldset');
+  const formMap = document.querySelector('.map__filters');
+  const collectionOptionsMap = formMap.querySelectorAll('select');
 
-//   adForm.classList.add('ad-form--disabled');
-//   formMap.classList.add('ad-form--disabled')
-//   collectionFieldsetNotice.forEach((value) => value.disabled = true );
-//   collectionOptionsMap.forEach((value) => value.disabled = true)
-// };
+  adForm.classList.add('ad-form--disabled');
+  formMap.classList.add('ad-form--disabled')
+  collectionFieldsetNotice.forEach((value) => value.disabled = true );
+  collectionOptionsMap.forEach((value) => value.disabled = true)
+};
 
-// const enabledMap = function () {
-//   const adForm = document.querySelector('.ad-form');
-//   const collectionFieldsetNotice = adForm.querySelectorAll('fieldset');
-//   const formMap = document.querySelector('.map__filters');
-//   const collectionOptionsMap = formMap.querySelectorAll('select');
+const enabledMap = function () {
+  const adForm = document.querySelector('.ad-form');
+  const collectionFieldsetNotice = adForm.querySelectorAll('fieldset');
+  const formMap = document.querySelector('.map__filters');
+  const collectionOptionsMap = formMap.querySelectorAll('select');
 
-//   adForm.classList.remove('ad-form--disabled');
-//   formMap.classList.remove('ad-form--disabled')
-//   collectionFieldsetNotice.forEach((value) => value.disabled = false );
-//   collectionOptionsMap.forEach((value) => value.disabled = false)
-// };
+  adForm.classList.remove('ad-form--disabled');
+  formMap.classList.remove('ad-form--disabled')
+  collectionFieldsetNotice.forEach((value) => value.disabled = false );
+  collectionOptionsMap.forEach((value) => value.disabled = false)
+};
